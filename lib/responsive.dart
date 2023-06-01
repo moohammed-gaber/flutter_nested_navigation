6f5d03be6386fc9e31f9493a22a188fd15e1d8d5
Mohamed Gaber <moohammed.gaber@gmail.com>
2023-06-01 16:57:02 +0300

    ..

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 class Responsive {
   bool isThreeBarsMode(BuildContext context) {
     return context.width > 1300;
   }
+
   bool isOnlyUserImageMode(BuildContext context) {
     return context.width < 550;
   }
 
+  bool get isWebMode => kIsWeb;
 }
 
 extension BuildContextExtenstions on BuildContext {
--- lib/root.dart
+++ lib/root.dart
@@ -1,14 +1,10 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 import 'package:nested_navigation/chat.dart';
-import 'package:nested_navigation/controller.dart';
 import 'package:nested_navigation/end_bar.dart';
-import 'package:nested_navigation/entities.dart';
 import 'package:nested_navigation/injection.dart';
 import 'package:nested_navigation/responsive.dart';
 import 'package:nested_navigation/rooms.dart';
-import 'package:nested_navigation/root.dart';
+import 'package:universal_html/html.dart'as html;
 
 class NestedNavigationPage extends StatefulWidget {
   NestedNavigationPage({Key? key}) : super(key: key);
@@ -20,63 +16,55 @@ class NestedNavigationPage extends StatefulWidget {
 class _NestedNavigationPageState extends State<NestedNavigationPage> {
   @override
   Widget build(BuildContext context) {
-    print(responsive.isThreeBarsMode(context));
+    final userAgent = html.window.navigator.userAgent.toString().toLowerCase();
+
+    print(userAgent);
     return ValueListenableBuilder(
       valueListenable: controller,
       builder: (context, value, child) => Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                context: navigation.chatKey.currentContext!,
-                builder: (x) {
-                  return Material(
-                    child: Column(),
-                    color: Colors.red,
-                  );
-                });
-          },
-        ),
-        body: Column(
-          children: [
-            Text(
-              'Height ${context.height} - Width ${context.width}',
-              style: TextStyle(fontSize: 20),
-            ),
-            Expanded(
-              child: Row(children: [
-                Expanded(
-                  child: Rooms(),
-                ),
-                VerticalDivider(width: 0),
-                Expanded(
-                  flex: 3,
-                  child: Stack(
-                    alignment: AlignmentDirectional.centerEnd,
-                    children: [
-                      Navigator(
-                        key: navigation.chatKey,
-                        onGenerateRoute: navigation.onGenerateChatRoute,
-                        initialRoute: IdleChat.route,
+        body: responsive.isWebMode
+            ? Column(
+                children: [
+                  Text(
+                    'Height ${context.height} - Width ${context.width}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Expanded(
+                    child: Row(children: [
+                      Expanded(
+                        child: Rooms(),
                       ),
+                      VerticalDivider(width: 0),
+                      Expanded(
+                        flex: 3,
+                        child: Stack(
+                          alignment: AlignmentDirectional.centerEnd,
+                          children: [
+                            Navigator(
+                              key: navigation.chatKey,
+                              onGenerateRoute: navigation.onGenerateChatRoute,
+                              initialRoute: IdleChat.route,
+                            ),
+                            Visibility(
+                              child: EndBar(),
+                              visible: !responsive.isThreeBarsMode(context),
+                            )
+                          ],
+                        ),
+                      ),
+                      VerticalDivider(width: 0),
+
                       Visibility(
                         child: EndBar(),
-                        visible: !responsive.isThreeBarsMode(context),
+                        visible: responsive.isThreeBarsMode(context),
                       )
-                    ],
+
+                      // EndBar()
+                    ]),
                   ),
-                ),
-                VerticalDivider(width: 0),
-
-                Visibility(
-                  child: EndBar(),
-                  visible: responsive.isThreeBarsMode(context),
-                )
-
-                // EndBar()
-              ]),
-            ),
-          ],
-        ),
+                ],
+              )
+            : Rooms(),
       ),
     );
   }
